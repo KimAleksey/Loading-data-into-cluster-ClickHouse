6d9fc40cfdfe3a63512c64a605ec12fa468c85c1
AlekseiKim <kimalex8589@gmail.com>
2026-01-06 22:08:24 +0300

    Скрипт для загрузки данных

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
-def main():
-    print("Hello from project-2-simple-load-to-ch!")
+import random
 
+import clickhouse_connect
+
+from pandas import DataFrame
+
+def main():
+    try:
+        client = clickhouse_connect.get_client(
+            host="localhost",
+            port=8123,
+            user="default",
+            password="default",
+        )
+    except Exception as error:
+        raise RuntimeError(error)
+
+    data = {}
+
+    for i in range(1000000):
+        data[i] = chr(random.randint(0, 26) + 97)
+
+    df = DataFrame(list(data.items()), columns=["x", "y"])
+
+    client.insert_df(
+        database="default",
+        table="table_distr",
+        df=df,
+    )
 
 if __name__ == "__main__":
     main()
